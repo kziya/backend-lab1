9cbfe0cc0d827f1662a4b4259d68c47cdca27bc0
Ziya Karimli <ziyakerimli04@gmail.com>
2023-10-08 14:01:59 +0300

    Create app files

---
+++ src/views.py
@@ -0,0 +1,11 @@
+from src import app
+
+import json
+import datetime
+@app.route('/healthcheck')
+def healthCheck():
+
+    return json.dumps({
+        'date': str(datetime.datetime.now()),
+        'status': True
+    })
